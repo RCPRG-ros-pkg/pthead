ad3bd1cafcc2211e6e7da85aeedae220cbb90db7
Michal Walecki <mw@mwalecki.pl>
2012-12-14 20:34:59 +0100

    first commit

---
+++ src/pthead.cpp
@@ -0,0 +1,122 @@
+#include "ros/ros.h"
+#include "sensor_msgs/JointState.h"
+#include "ptproxy/PTProxy.h"
+#include <stdio.h>
+#include <cstring>
+
+std::string nodeName = "pthead";
+
+/**
+* 
+*/
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, nodeName);
+	ros::NodeHandle n;
+
+	ros::Publisher js_pub = n.advertise<sensor_msgs::JointState>("/joint_states", 10);
+	ros::Rate loop_rate(10);
+
+	std::string js_names[]	= {"head_pan_joint", "head_tilt_joint"};
+	double 	js_pos[]	= {0, 0};
+	double 	js_vel[]	= {0, 0};
+	double 	js_effort[]	= {0, 0};
+
+	ROS_INFO("Starting node %s", nodeName.c_str());
+	
+	// get the console in raw mode
+	char inp;
+	float dx = 0, dy = 0, rx, ry;
+	bool synchronized = false;
+	int kfd = 0;
+	struct termios cooked, raw;
+	tcgetattr(kfd, &cooked);
+	memcpy(&raw, &cooked, sizeof(struct termios));
+	raw.c_lflag &= ~(ICANON);
+	raw.c_lflag &= ~(ECHO);
+	raw.c_cc[VEOL] = 1;
+	raw.c_cc[VEOF] = 2;
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 0;
+	tcsetattr(kfd, TCSANOW, &raw);
+	// read() is non-blocking from now on and echo is off  
+	
+	PTProxy *ptp = new PTProxy("/dev/ttyACM0");
+	// Prepare synchronization mode request
+	ptp->startSynchronization();
+	// Send it to motor controllers
+	// ptp->nextStep();
+	
+	printf("Pan-tilt speed control: [A] x- [D] x+ [S] y- [W] y+ [space] stop\n\n");
+	printf("Quit: [Q]\n\n");
+	sleep(1);
+	
+	bool quit = false;
+	while (ros::ok() && (!quit))
+	{	// Prepare data to send
+		if(read(0, (void*)&inp, 1)){
+			switch(inp){
+				case 'a':
+				case 'A':
+					dx -= 1;
+					ptp->setMotorSpeed(dx, dy);
+					break;
+				case 'd':
+				case 'D':
+					dx += 1;
+					ptp->setMotorSpeed(dx, dy);
+					break;
+				case 's':
+				case 'S':
+					dy -= 1;
+					ptp->setMotorSpeed(dx, dy);
+					break;
+				case 'w':
+				case 'W':
+					dy += 1;
+					ptp->setMotorSpeed(dx, dy);
+					break;
+				case ' ':
+					dx = 0;
+					dy = 0;
+					ptp->setMotorSpeed(dx, dy);
+					break;
+				case 'q':
+				case 'Q':
+					ptp->setMotorSpeed(0, 0);
+					quit = true;
+					break;
+			}
+		}
+		// Read - write hardware
+		ptp->nextStep();
+		// Process received data
+		ptp->getMotorPosition(rx, ry);
+		printf("px: %6d, py: %6d, syn: %d\n", (int)rx, (int)ry, ptp->isSynchronized());
+		
+		js_pos[0] = rx;
+		js_pos[1] = ry;
+		
+		// Building message
+		sensor_msgs::JointState msg;
+		msg.name = std::vector<std::string>(js_names, js_names+2);
+		msg.position = std::vector<double>(js_pos, js_pos+2);
+		msg.velocity = std::vector<double>(js_vel, js_vel+2);
+		msg.effort = std::vector<double>(js_effort, js_effort+2);
+		js_pub.publish(msg);
+
+		ros::spinOnce();
+
+		loop_rate.sleep();
+	}
+
+	// restore console settings
+	raw.c_lflag |= ECHO | ICANON;
+	raw.c_cc[VEOL] = 1;
+	raw.c_cc[VEOF] = 2;
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 0;
+	tcsetattr(kfd, TCSANOW, &raw);
+
+	return 0;
+}
