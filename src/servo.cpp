93f51d9b228c6ed009d7e4b7d3a9412d15717b4b
Michal Walecki <mw@mwalecki.pl>
2013-01-07 13:35:16 +0100

    agent structure of the whole servo application

--- src/servo.cpp
+++ src/servo.cpp
@@ -1,6 +1,8 @@
 #include "ros/ros.h"
 #include <stdio.h>
 #include <cstring>
+#include "sensor_msgs/Joy.h"
+#include "sensor_msgs/JointState.h"
 #include "geometry_msgs/Twist.h"
 #include "geometry_msgs/PointStamped.h"
 
@@ -11,22 +13,30 @@
 #include "opencv2/core/core_c.h"
 #include "opencv2/highgui/highgui_c.h"
 
-#define COEFF_P			4.0
-#define ANGLE_INSENS	0.25
-#define INCREM_LIMIT	70.0
+#include "psdefs.h"
+
+#define COEFF_P			250.0
+#define ANGLE_INSENS	0.05
+#define INCREM_LIMIT	80.0
 
 std::string nodeName = "ptservo";
 
-//bool trackerPaused	= false;
-//bool trackerLog	= false;
+FILE * logfile;
+
+bool trackerPaused	= false;
+bool trackerLog	= false;
 //bool pthSynchronize	= false;
 
+ros::Subscriber pos_sub;
+ros::Subscriber joy_sub;
+ros::Subscriber joints_sub;
+ros::Publisher twist_pub;
 
 /**
 * 
 */
 float insensitive(float v, float lvl) {
-	lvl = fabs(lvl)
+	lvl = fabs(lvl);
 	return (v > lvl ? (v - lvl) : (v < (-lvl) ? (v + lvl) : 0) );
 }
 
@@ -40,10 +50,50 @@ float inrange(float v, float mn, float mx) {
 /**
 * 
 */
+void joyCallback(const sensor_msgs::Joy& msg) {
+	static sensor_msgs::Joy prevmsg;
+
+	if(msg.buttons[PS3_BUTTON_REAR_RIGHT_1] == 1
+			&& prevmsg.buttons[PS3_BUTTON_REAR_RIGHT_1] == 0){
+		if(trackerPaused) {
+			trackerPaused = false;
+			ROS_INFO("PSMove Tracker RESUMED");
+		}
+		else {
+			
+			trackerPaused = true;
+			geometry_msgs::Twist	msgTwist;
+			msgTwist.angular.z = 0;
+			msgTwist.angular.y = 0;
+			twist_pub.publish(msgTwist);
+			ROS_INFO("PSMove Tracker PAUSED");
+		}
+	}
+
+	if(msg.buttons[PS3_BUTTON_ACTION_SQUARE] == 1
+			&& prevmsg.buttons[PS3_BUTTON_ACTION_SQUARE] == 0){
+		if(trackerLog) {
+			trackerLog = false;
+			ROS_INFO("Logging OFF");
+		}
+		else {
+			trackerLog = true;
+			ROS_INFO("Logging ON");
+		}
+	}
+	
+	prevmsg = msg;
+}
+
+/**
+* 
+*/
 void posCallback(const geometry_msgs::PointStamped& msg) {
+	if(trackerPaused)
+		return;
 	float xx, yy;
-	xx = msg->point.x;
-	yy = msg->point.y;
+	xx = msg.point.x;
+	yy = msg.point.y;
 	
 	xx = insensitive(xx, ANGLE_INSENS);
 	yy = insensitive(yy, ANGLE_INSENS);
@@ -56,6 +106,16 @@ void posCallback(const geometry_msgs::PointStamped& msg) {
 	msgTwist.angular.z = xx;
 	msgTwist.angular.y = yy;
 	twist_pub.publish(msgTwist);
+	
+	//if(trackerLog)
+	//    fprintf (logfile, "rotx %5f, roty %5f \n", msg.point.x, msg.point.y);
+}
+
+/**
+* 
+*/
+void jstatesCallback(const sensor_msgs::JointState& msg) {
+
 }
 
 /**
@@ -66,8 +126,12 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, nodeName);
 	ros::NodeHandle n;
 
-	ros::Subscriber pos_sub = n.subscribe("pos_error", 1, &posCallback);
-	ros::Publisher twist_pub = n.advertise<geometry_msgs::Twist>("head_vel", 10);
+	pos_sub = n.subscribe("pos_error", 1, &posCallback);
+	joints_sub = n.subscribe("head_jstates", 1, &jstatesCallback);
+	joy_sub = n.subscribe("psmove_out", 1, &joyCallback);
+	twist_pub = n.advertise<geometry_msgs::Twist>("head_vel", 10);
+	
+	//logfile = fopen ("pstrackerlog.txt","a");
 
 	ROS_INFO("Starting node %s", nodeName.c_str());
 	
--- src/tracker.cpp
+++ src/tracker.cpp
@@ -15,49 +15,7 @@
 #include <psmove.h>
 #include <psmove_tracker.h>
 
-// note on plain values:
-// buttons are either 0 or 1
-// button axes go from 0 to -1
-// stick axes go from 0 to +/-1
-
-#define PS3_BUTTON_SELECT            0
-#define PS3_BUTTON_STICK_LEFT        1
-#define PS3_BUTTON_STICK_RIGHT       2
-#define PS3_BUTTON_START             3
-#define PS3_BUTTON_CROSS_UP          4
-#define PS3_BUTTON_CROSS_RIGHT       5
-#define PS3_BUTTON_CROSS_DOWN        6
-#define PS3_BUTTON_CROSS_LEFT        7
-#define PS3_BUTTON_REAR_LEFT_2       8
-#define PS3_BUTTON_REAR_RIGHT_2      9
-#define PS3_BUTTON_REAR_LEFT_1       10
-#define PS3_BUTTON_REAR_RIGHT_1      11
-#define PS3_BUTTON_ACTION_TRIANGLE   12
-#define PS3_BUTTON_ACTION_CIRCLE     13
-#define PS3_BUTTON_ACTION_CROSS      14
-#define PS3_BUTTON_ACTION_SQUARE     15
-#define PS3_BUTTON_PAIRING           16
-
-#define PS3_AXIS_STICK_LEFT_LEFTWARDS    0
-#define PS3_AXIS_STICK_LEFT_UPWARDS      1
-#define PS3_AXIS_STICK_RIGHT_LEFTWARDS   2
-#define PS3_AXIS_STICK_RIGHT_UPWARDS     3
-#define PS3_AXIS_BUTTON_CROSS_UP         4
-#define PS3_AXIS_BUTTON_CROSS_RIGHT      5
-#define PS3_AXIS_BUTTON_CROSS_DOWN       6
-#define PS3_AXIS_BUTTON_CROSS_LEFT       7
-#define PS3_AXIS_BUTTON_REAR_LEFT_2      8
-#define PS3_AXIS_BUTTON_REAR_RIGHT_2     9
-#define PS3_AXIS_BUTTON_REAR_LEFT_1      10
-#define PS3_AXIS_BUTTON_REAR_RIGHT_1     11
-#define PS3_AXIS_BUTTON_ACTION_TRIANGLE  12
-#define PS3_AXIS_BUTTON_ACTION_CIRCLE    13
-#define PS3_AXIS_BUTTON_ACTION_CROSS     14
-#define PS3_AXIS_BUTTON_ACTION_SQUARE    15
-#define PS3_AXIS_ACCELEROMETER_LEFT      16
-#define PS3_AXIS_ACCELEROMETER_FORWARD   17
-#define PS3_AXIS_ACCELEROMETER_UP        18
-#define PS3_AXIS_GYRO_YAW                19
+#include "psdefs.h"
 
 std::string nodeName = "ptmovetracker";
 
@@ -85,7 +43,7 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, nodeName);
 	ros::NodeHandle n;
 
-	ros::Publisher twist_pub = n.advertise<geometry_msgs::Twist>("head_vel", 10);
+	//ros::Publisher twist_pub = n.advertise<geometry_msgs::Twist>("head_vel", 10);
 	ros::Publisher psmove_pub = n.advertise<sensor_msgs::Joy>("psmove_out", 10);
 	ros::Publisher ballpos_pub = n.advertise<geometry_msgs::PointStamped>("ball_position", 10);
 	ros::Rate loop_rate(50);
