8da5a300634e9fc48d37c1c2426b6ffa55347028
Michal Walecki <mw@mwalecki.pl>
2013-05-02 14:59:07 +0200

    first successful moves

--- src/pthead.cpp
+++ src/pthead.cpp
@@ -13,7 +13,8 @@
 #include "sensor_msgs/Joy.h"
 #include "psdefs.h"
 
-#define MOVE_TO_CENTER_SPEED	1.0 //[rad/s]
+#define MOVE_TO_CENTER_VELOCITY	1.0 //[rad/s]
+#define MAX_TRAJECTORY_VELOCITY	2.0 //[rad/s]
 #define SPIN_FREQ				100
 
 std::string nodeName = "pthead";
@@ -21,12 +22,11 @@ PTProxy *ptp;
 float dx = 0, dy = 0;
 
 std::vector<VelocityProfile_Spline> vel_profile_;
-trajectory_msgs::JointTrajectoryPoint trajectory_current_;
-trajectory_msgs::JointTrajectoryPoint trajectory_next_;
-std::vector<double> des_jnt_pos_;
+trajectory_msgs::JointTrajectoryPoint current_trajectory_point;
+trajectory_msgs::JointTrajectoryPoint next_trajectory_point;
 unsigned int number_of_joints_ = 2;
 bool trajectory_ready_;
-bool buffer_ready_;
+bool buffer_empty_ = true;
 int64_t time_;
 int64_t end_time_;
 double dt_;
@@ -51,7 +51,9 @@ void twistCallback(const geometry_msgs::TwistStamped& msg) {
 */
 void trajectoryCallback(const trajectory_msgs::JointTrajectory& msg) {
 	ROS_WARN("trajectory callback in pthead");
-
+	
+	next_trajectory_point = msg.points[1];
+	buffer_empty_ = false;
 }
 
 /**
@@ -61,7 +63,7 @@ void joyCallback(const sensor_msgs::Joy& msg) {
 	static sensor_msgs::Joy prevmsg;
 	if(msg.buttons[PS3_BUTTON_ACTION_CROSS] == 1
 			&& prevmsg.buttons[PS3_BUTTON_ACTION_CROSS] == 0){
-		ptp->setJointPositionWithSpeed(0, 0, MOVE_TO_CENTER_SPEED, MOVE_TO_CENTER_SPEED);	
+		ptp->setJointPositionWithSpeed(0, 0, MOVE_TO_CENTER_VELOCITY, MOVE_TO_CENTER_VELOCITY);
 	}
 	prevmsg = msg;
 }
@@ -132,7 +134,6 @@ int main(int argc, char **argv)
 	ptp = new PTProxy("/dev/ttyACM0");
 	
 	// Trajectory generation
-    des_jnt_pos_.resize(number_of_joints_);
     vel_profile_.resize(number_of_joints_);
     joint_names_.resize(number_of_joints_);
 	joint_names_[0] = "head_pan_joint";
@@ -140,12 +141,12 @@ int main(int argc, char **argv)
 	q.resize(number_of_joints_);
 	qd.resize(number_of_joints_);
 	qdd.resize(number_of_joints_);
-    trajectory_current_.positions.reserve(number_of_joints_);
-    trajectory_current_.velocities.reserve(number_of_joints_);
-    trajectory_current_.accelerations.reserve(number_of_joints_);
-    trajectory_next_.positions.reserve(number_of_joints_);
-    trajectory_next_.velocities.reserve(number_of_joints_);
-    trajectory_next_.accelerations.reserve(number_of_joints_);
+    current_trajectory_point.positions.resize(number_of_joints_);
+    current_trajectory_point.velocities.resize(number_of_joints_);
+    current_trajectory_point.accelerations.resize(number_of_joints_);
+    next_trajectory_point.positions.resize(number_of_joints_);
+    next_trajectory_point.velocities.resize(number_of_joints_);
+    next_trajectory_point.accelerations.resize(number_of_joints_);
 	trajectory_state_msg.desired.positions.resize(number_of_joints_);
 	trajectory_state_msg.desired.velocities.resize(number_of_joints_);
 	trajectory_state_msg.desired.accelerations.resize(number_of_joints_);
@@ -164,8 +165,99 @@ int main(int argc, char **argv)
 	int spinCnt = 1;
 	while (ros::ok())
 	{
-		// Calculate desired trajectory point
-		//q = ...
+		// If local trajectory in progress...
+		if (trajectory_ready_)
+		{
+			// Calculate desired trajectory point
+			for (unsigned int j = 0; j < number_of_joints_; j++){
+				q[j] = vel_profile_[j].Pos(time_ * dt_);
+			}
+			// And command it to motion controllers
+			ptp->setJointPositionWithSpeed(q[0], q[1], MAX_TRAJECTORY_VELOCITY, MAX_TRAJECTORY_VELOCITY);
+
+			// If local trajectory execution is about to end...
+			if (time_++ >= end_time_)
+			{
+				// If global trajectory points await in buffer
+				if (!buffer_empty_)
+				{
+					// Calculate new local trajectory coefficients
+					for (unsigned int j = 0; j < number_of_joints_; ++j)
+					{
+						// Using accelerations, if available
+						if (current_trajectory_point.accelerations.size() > 0 && next_trajectory_point.accelerations.size() > 0){
+							vel_profile_[j].SetProfileDuration(
+								current_trajectory_point.positions[j], current_trajectory_point.velocities[j], current_trajectory_point.accelerations[j],
+								next_trajectory_point.positions[j], next_trajectory_point.velocities[j], next_trajectory_point.accelerations[j],
+								next_trajectory_point.time_from_start.toSec());
+						}
+						// Using velocities, if available
+						else if (current_trajectory_point.velocities.size() > 0 && next_trajectory_point.velocities.size() > 0){
+							vel_profile_[j].SetProfileDuration(
+								current_trajectory_point.positions[j], current_trajectory_point.velocities[j],
+								next_trajectory_point.positions[j], next_trajectory_point.velocities[j],
+								next_trajectory_point.time_from_start.toSec());
+						}
+						// Or based only on positions
+						else{
+							vel_profile_[j].SetProfileDuration(current_trajectory_point.positions[j],
+								next_trajectory_point.positions[j], next_trajectory_point.time_from_start.toSec());
+						}
+					}
+					// Calculate new local trajectory duration
+					end_time_ = next_trajectory_point.time_from_start.toSec()/dt_;
+					time_ = 0;
+					current_trajectory_point = next_trajectory_point;
+					buffer_empty_ = true;
+				}
+				else
+				{
+					trajectory_ready_ = false;
+				}
+			}
+		}
+		// If NO local trajectory in progress...
+		else {
+			// If global trajectory points await in buffer
+			if (!buffer_empty_)
+			{
+				// Make currently processed global trajectory point out of current readings
+				for (unsigned int j = 0; j < number_of_joints_; j++) {
+					current_trajectory_point.positions[j] = meas_q[j];
+					current_trajectory_point.velocities[j] = 0.0; // meas_qd[j];
+					current_trajectory_point.accelerations[j] = 0.0;
+				}
+				// Calculate new local trajectory coefficients
+				for (unsigned int j = 0; j < number_of_joints_; ++j)
+				{
+					// Using accelerations, if available
+					if (current_trajectory_point.accelerations.size() > 0 && next_trajectory_point.accelerations.size() > 0){
+						vel_profile_[j].SetProfileDuration(
+							current_trajectory_point.positions[j], current_trajectory_point.velocities[j], current_trajectory_point.accelerations[j],
+							next_trajectory_point.positions[j], next_trajectory_point.velocities[j], next_trajectory_point.accelerations[j],
+							next_trajectory_point.time_from_start.toSec());
+					}
+					// Using velocities, if available
+					else if (current_trajectory_point.velocities.size() > 0 && next_trajectory_point.velocities.size() > 0){
+						vel_profile_[j].SetProfileDuration(
+							current_trajectory_point.positions[j], current_trajectory_point.velocities[j],
+							next_trajectory_point.positions[j], next_trajectory_point.velocities[j],
+							next_trajectory_point.time_from_start.toSec());
+					}
+					// Or based only on positions
+					else{
+						vel_profile_[j].SetProfileDuration(current_trajectory_point.positions[j],
+							next_trajectory_point.positions[j], next_trajectory_point.time_from_start.toSec());
+					}
+				}
+				// Calculate new local trajectory duration
+				end_time_ = next_trajectory_point.time_from_start.toSec()/dt_;
+				time_ = 0;
+				current_trajectory_point = next_trajectory_point;
+				buffer_empty_ = true;
+				trajectory_ready_ = true;
+			}
+		}
 
 		// Get timestamp
 		ros::Time now = ros::Time::now();
